c98ec29a3f06b920a1a73c6e26745fe2b94a8ed5
jdpmm <jdpm.code@gmail.com>
2022-06-29 19:39:46 -0500

    better Double linked list: C

--- introducion/b-dll.c
+++ introducion/b-dll.c
@@ -4,187 +4,145 @@
 struct Node {
     int value;
     int index;
-    struct Node* left;
-    struct Node* right;
+    struct Node* prev;
+    struct Node* next;
 };
-int n_nodes = 0;
+int length = 0;
 
-struct Node* dll_make (int value, int index, struct Node* left, struct Node* right)
+struct Node* dll_make (int value, struct Node* prev, struct Node* next)
 {
-    struct Node* new_n = malloc(sizeof(struct Node));
-    new_n->value = value;
-    new_n->index = index;
-    new_n->left = left; 
-    new_n->right = right; 
+    struct Node* new_node = malloc(sizeof(struct Node*));
+    new_node->value = value;
+    new_node->index = length;
+    new_node->prev = prev;
+    new_node->next = next;
 
-    n_nodes++;
-    return new_n;
+    length++;
+    return new_node;
 }
 
-void dll_update_indexes (struct Node* from, int set)
+void dll_update_indexes (struct Node* root, int add)
 {
     /**
-     * When some element is pushed or deleted at the middle
-     * or at the begin (only pop operation) the indexes
-     * must be updated to make future oprations.
+     * When a node is inserted or deleted
+     * at the start or at the middle the next
+     * indexes nodes must be updated.
      * **/
-    struct Node* updaten = from;
-    while ( updaten ) {
-        updaten->index += set;
-        updaten = updaten->right;
+    while ( root ) {
+        root->index += add;
+        root = root->next;
     }
 }
 
-void dll_print (struct Node* main)
+void dll_pop (struct Node** root, int index)
 {
-    /**
-     * Prints the value and the index:
-     * (value index)
-     * **/
-    struct Node* auxp = main;
-    while ( auxp ) {
-        printf("(%d %d), ", auxp->value, auxp->index);
-        auxp = auxp->right;
+    if ( index >= length || index <= -1 ) {
+        printf("Unrechable index: %d\n", index);
+        return;
     }
-    printf("null.\n");
-}
-
-void dll_push (struct Node* main, int value, int index)
-{
-    if ( index > n_nodes || index <= -1 ) {
-        printf("Unreachable index: %d\n", index);
+    if ( !index && length == 1 ) {
+        printf("Only there is one node, could not be deleted.\n");
         return;
     }
 
-    struct Node* insertn = main;
-    while ( insertn ) {
-        if ( insertn->index == index ) { break; }
-        insertn = insertn->right;
+    struct Node* insert_n = (*root);
+    while ( insert_n ) {
+        if ( insert_n->index == index ) { break; }
+        insert_n = insert_n->next;
     }
 
-    if ( !insertn->right ) {
-        /**
-         * When there is not another node to the right
-         * in the current node means the current
-         * node is the last one on the list.
-         * **/
-        insertn->right = dll_make(value, index + 1, insertn, NULL);
+    if ( !index ) {
+        (*root) = (*root)->next;
+        (*root)->prev = NULL;
+        dll_update_indexes(insert_n, -1);
+    }
+    else if ( index == (length - 1) ) {
+        insert_n->prev->next = NULL;
+        insert_n->prev = NULL;
     }
     else {
-        struct Node* rightn = insertn->right;
-        /**
-         * All nodes must be updated (index value)
-         * from the next node until the last one, its index value
-         * must be incremented by 1.
-         * **/
-        dll_update_indexes(rightn, 1);
-
-        /**
-         * At the begin: [1 0] = [2 1] = [3 2]
-         *                 \ maybe here is pointing insertn, and maybe the value
-         *                 5 will be inserted.
-         * Then: [1 0] = [5 1] = [2 2] = [3 3]
-         * **/
-        insertn->right = dll_make(value, index + 1, insertn, NULL);
-        insertn->right->right = rightn;
-        rightn->left = insertn->right;
+        dll_update_indexes(insert_n->next, -1);
+        insert_n->next->prev = insert_n->prev;
+        insert_n->prev->next = insert_n->next;
+        insert_n->next = NULL;
+        insert_n->prev = NULL;
     }
+    free (insert_n);
+    length--;
 }
 
-void dll_pop (struct Node** main, int index)
+void dll_push (struct Node** root, int value, int index)
 {
-    if ( index > n_nodes || index <= -1 ) {
-        printf("Unreachable index: %d\n", index);
-        return;
-    }
-    /**
-     * When only there is one node, that node could not be deleted
-     * because there will be not a reference to the list then.
-     * **/
-    if ( !index && n_nodes == 1 ) {
-        printf("Only there is one node, could not be deleted\n");
+    if ( index >= length || index <= -2  ) {
+        printf("Unrechable index: %d\n", index);
         return;
     }
 
-    /**
-     * The pop function needs the original main node, not a reference.
-     * **/
-    struct Node* popn = (*main);
-    while ( popn ) {
-        if ( popn->index == index ) { break; }
-        popn = popn->right;
+    struct Node* insert_n = (*root);
+    while ( insert_n ) {
+        if ( insert_n->index == index ) { break; }
+        insert_n = insert_n->next;
     }
 
-    if ( !popn->right ) {
-        /**
-         * Remove the last one:
-         * At the begin: [3 0] = [4 1] = [1 3]
-         * Then: [3 0] = [4 1]
-         * **/
-        popn->left->right = NULL;
-        popn->left = NULL;
+    if ( index == -1 ) {
+        (*root)->prev = dll_make(value, NULL, *root);
+        (*root) = (*root)->prev;
+        (*root)->index = 0;
+        dll_update_indexes((*root)->next, 1);
     }
-    else if ( !index ) {
-        /**
-         * Remove the first one:
-         * At the begin: [1 0] = [2 1] = [3 2]
-         * Then: [2 0] = [3 1]
-         * **/
-        dll_update_indexes(popn->right, -1);
-        (*main) = popn->right;
-        (*main)->left = NULL;
-        popn->right = NULL;
+    else if ( index == (length - 1) ) {
+        insert_n->next = dll_make(value, insert_n, NULL);
     }
     else {
-        /**
-         * Remove one node at the midddle:
-         * At the begin: [1 0] = [2 1] = [3 2]
-         * Then: [1 0] = [3 1]
-         * **/
-        dll_update_indexes(popn->right, -1);
-        popn->right->left = popn->left;
-        popn->left->right = popn->right;
-        popn->right = NULL;
-        popn->left = NULL;
+        dll_update_indexes(insert_n->next, 1);
+        insert_n->next = dll_make(value, insert_n, insert_n->next);
+        insert_n->next->next->prev = insert_n->next;
+        insert_n->next->index = insert_n->index + 1;
     }
-
-    free(popn);
-    n_nodes--;
 }
 
-void dll_edit (struct Node* main, int newval, int index)
+void dll_print (struct Node* root)
 {
-    if ( index > n_nodes || index <= -1 ) {
-        printf("Unreachable index: %d\n", index);
+    while ( root ) {
+        printf("[%d %d], ", root->value, root->index);
+        root = root->next;
+    }
+    printf("null\n");
+}
+
+void dll_edit (struct Node* root, int new_value, int index)
+{
+    if ( index >= length || index <= -1 ) {
+        printf("Unrechable index: %d\n", index);
         return;
     }
-    struct Node* editn = main;
-    while ( editn ) {
-        if ( editn->index == index ) { break; }
-        editn = editn->right;
+
+    while ( root ) {
+        if ( root->index == index ) { break; }
+        root = root->next;
     }
-    editn->value = newval;
+    root->value = new_value;
 }
 
 int main ()
 {
-    struct Node* main = dll_make(1, 0, NULL, NULL);
-    dll_push(main, 2, 0);
-    dll_push(main, 3, 1);
-    dll_push(main, 4, 2);
-    dll_push(main, 5, 3);
-    dll_print(main);
+    struct Node* main_node = dll_make(2, NULL, NULL);
+    dll_push(&main_node, 4, -1);
+    dll_push(&main_node, 8, 1);
+    dll_push(&main_node, 9, 1);
+    dll_print(main_node);
 
-    dll_edit(main, 16, 0);
-    dll_print(main);
+    dll_pop(&main_node, 0);
+    dll_push(&main_node, 16, -1);
+    dll_push(&main_node, 16, length - 1);
+    dll_edit(main_node, -16, length - 1);
+    dll_print(main_node);
 
-    dll_pop(&main, 0);
-    dll_push(main, -15, 0);
-    dll_print(main);
+    dll_pop(&main_node, length - 1);
+    dll_pop(&main_node, 0);
+    dll_pop(&main_node, length - 1);
+    dll_pop(&main_node, 0);
 
-    dll_pop(&main, n_nodes - 1);
-    dll_pop(&main, 2);
-    dll_print(main);
+    dll_print(main_node);
     return 0;
 }
-
